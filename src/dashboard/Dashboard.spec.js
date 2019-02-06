7049686b24bff8ac87b112cae425574b645c6458
louislee7 <7.louislee@gmail.com>
2019-02-06 12:22:37 -0800

    dashboard tests done

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from 'react';
+import { render, cleanup } from "react-testing-library";
+import Display from "../display/Display";
+import Controls from '../controls/Controls';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+describe('the Dashboard component', () => {
+    it('render the Display component', () => {
+        render(<Display />);
+    })
+
+    it('render the Controls component', () => {
+        render(<Controls />);
+    })
+})
